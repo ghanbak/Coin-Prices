8a939ee2804f158656e6138fac6a6a5e91919315
Ethan MacDonald <78871177+ethan-bc@users.noreply.github.com>
2022-07-21 13:51:29 -0700

    Initial Commit

---
+++ Coin Prices/Coin_PricesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Coin_PricesApp.swift
+//  Coin Prices
+//
+//  Created by Ethan MacDonald on 7/21/22.
+//
+
+import SwiftUI
+
+@main
+struct Coin_PricesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Coin Prices/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Coin Prices
+//
+//  Created by Ethan MacDonald on 7/21/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
